5963fe87e958d28d1c06e914c88c220b75be0f27
Shreya Tiwari <your_email@example.com>
2025-12-02 00:17:05 +0530

    app.js file updated with configurations

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 import express from 'express';
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 //configurations done after app making
 const app = express()
 
 //configuring cors
-// app.use(cors())      // use() se middlewares,configuration hoti h
+// app.use(cors())      // app.use() se middlewares,configuration settings hoti h
 app.use(cors({
-    //states which origin to allow taking data from databse
-    origin: 
+    //states which origin to allow taking data from database
+    origin: process.env.CORS_ORIGIN,
+    credentials:true       // there are more options
 }))
 
+//configuration -> for datacoming from forms
+app.use(express.json({limit:"16kb"}))
+
+//configuration ->for data coming from url
+app.use(express.urlencoded({extended:true}))
+
+//configuration -> for data(image,pdf,video) that is to be kept inside folder "public"
+app.use(express.static("public"))
+
+//configuration -> for keeping secure cookies inside user's browser
+app.use(cookieParser())
 
 export { app }   //another way to export 
