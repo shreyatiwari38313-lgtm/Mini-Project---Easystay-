98ce3a461a6f8313f42040b86863bda580dd9bd6
Shreya Tiwari <your_email@example.com>
2025-12-01 17:46:06 +0530

    fourth commit done images videos upload code

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from "cloudinary";
+
+import fs from "fs";      // fs stands for file system
+
+//configuration 
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    //we have to keep them(cloud_name,api_key,api_secret) in .env file
+
+    //uploading a file
+    const uploadOnCloud = async (localFilePath)=>{
+        try{
+           if(!localFilePath) return null;
+           const response = await cloudinary.uploader.upload(localFilePath,{
+             resource: auto 
+           })
+           console.log("file has been successfully uploaded", response.url);
+           return response;
+        }catch(error){
+          fs.unlinkSync(localFilePath)
+          return null;
+        }
+    }
